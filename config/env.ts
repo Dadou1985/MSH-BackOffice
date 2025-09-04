6a72022ce2a8b2481c3acdbe6430b7ced9d436e3
David Simba <david.simba1985@gmail.com>
2025-09-04 11:51:45 +0200

    google translate api implementation: done

--- config/env.ts
+++ config/env.ts
@@ -1,7 +1,7 @@
 import {config} from 'dotenv';
 
-const envPath = `.env.${process.env.NODE_ENV || 'development'}.local`;
+const envPath = `.env.${process.env.NODE_ENV || 'production'}.local`;
 config({ path: envPath });
 
 export const PORT = process.env.PORT || 3000;
-export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const NODE_ENV = process.env.NODE_ENV || 'production';
