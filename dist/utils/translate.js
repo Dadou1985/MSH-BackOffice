66cce5c69e6868b8e68e204868c7e8e06b30946f
David Simba <david.simba1985@gmail.com>
2025-09-04 11:26:05 +0200

    google translate api implementation: done

---
+++ dist/utils/translate.js
@@ -0,0 +1,16 @@
+import { v2 as Translate } from '@google-cloud/translate';
+const projectId = process.env.FIREBASE_PROJECT_ID; // remplace par ton ID réel
+const translate = new Translate.Translate({
+    projectId,
+    keyFilename: './google-translate-key.json', // chemin vers la clé JSON
+});
+export async function translateText(text, targetLang) {
+    try {
+        const [translation] = await translate.translate(text, targetLang);
+        return translation;
+    }
+    catch (error) {
+        console.error('Erreur traduction:', error);
+        throw new Error('Échec de traduction');
+    }
+}
